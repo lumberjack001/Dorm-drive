e2810ec8b5a24e81a6811f3d204605524778c923
Shem Itoya <rachet.clank001@gmail.com>
2024-06-02 08:09:58 +0100

    add nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavLinksComponent } from './partials/nav-links/nav-links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    NavLinksComponent
   ],
   imports: [
     BrowserModule,
